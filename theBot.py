2a647d71ca5317cd59d33901ab53b08048082865
molchu <59625229+molchu@users.noreply.github.com>
2020-01-07 16:56:56 -0600

    Update theBot.py

---
+++ theBot.py
@@ -0,0 +1,21 @@
+import json
+import requests
+import discord
+from discord.ext import commands
+import asyncio
+import time
+import subprocess
+import os
+
+desc = "Bot made by molchu and CodeWritten for a game called Hackers to make simple and complex calculations"
+
+with open('botToken.txt', 'r') as f:
+	TOKEN = f.read()
+
+bot = commands.Bot(command_prefix = ".", description=desc)
+
+@bot.event
+async def on_ready():
+    print("Up and running")
+
+bot.run(TOKEN)
