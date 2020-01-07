36524b9099af2ef198b7de64123e6e848c1981a0
molchu <59625229+molchu@users.noreply.github.com>
2020-01-07 16:14:42 -0600

    Update HackersPySim.py

--- HackersPySim.py
+++ HackersPySim.py
@@ -6,4 +6,4 @@ try:
 #Operations Area
 #Error Catchment
 except FileNotFoundError:
-    print ("Error, could not find 1 or more files required, please ensure all files are downloaded and in dir with the Py file
+    print ("Error, could not find 1 or more files required, please ensure all files are downloaded and in dir with the Py file")
