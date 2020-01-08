b1c1db1854312c6ac6f1e30c8acdde8a71632d92
SSwiftGamer440 <37124896+SSwiftGamer440@users.noreply.github.com>
2020-01-08 11:36:27 -0800

    Create theBot_KEEP_ICE_AMETHYSMS_EDITS_FAIL.py

---
+++ theBot_KEEP_ICE_AMETHYSMS_EDITS_FAIL.py
@@ -0,0 +1,41 @@
+import json
+import requests
+import discord
+from discord.ext import commands
+import asyncio
+import time
+import subprocess
+import os
+
+desc = "Bot made by molchu and CodeWritten for a game called Hackers to make simple and complex calculations"
+
+with open('botToken.txt', 'r') as f:
+	TOKEN = f.read()
+
+bot = commands.Bot(command_prefix = ".", description=desc)
+
+@bot.event
+async def on_ready():
+    print("Up and running")
+
+#the command below it a test command and adds no functions to the bot whatsoever
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Hello")
+
+@bot.command()
+async def listParameter(ctx, *, args):
+    argsList = args.split(' ')
+    try:
+        with open("{}.json".format(argsList[0]), "r") as f:
+            temp1 = json.load(f)
+            temp2 = temp1[argsList[1]]
+            try:
+                temp3 = temp2[argsList[2]]
+                await ctx.send("```{}```".format(temp3))
+            except: 
+                await ctx.send("```{}```".format(temp2))
+    except:
+        await ctx.send("Something is wrong. Probably a bug or something, Contact CodeWritten#4044 or molchu#2575")
+
+bot.run(TOKEN)
