8d310cd7f0139a5cf7df7f8b0d3d710c4a3d8522
SSwiftGamer440 <37124896+SSwiftGamer440@users.noreply.github.com>
2020-01-19 16:17:25 -0800

    test.py was irrelevant, removing...

--- test.py
+++
@@ -1,8 +0,0 @@
-#For testing purposes only. For final commits please go to HackersPySim.py
-
-import json
-woahh = open("worm.json", "r")
-woah = json.load(woahh)
-woahhhh = woah["DPS"]
-level = woahhhh["21"]
-print(level)
