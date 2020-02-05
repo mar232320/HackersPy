141b1ee4a5d8f4d09576fa6491e26a5dee477919
SSwiftGamer440 <37124896+SSwiftGamer440@users.noreply.github.com>
2020-02-05 12:19:13 -0800

    Update theBot.py

--- theBot.py
+++ theBot.py
@@ -257,12 +257,14 @@ async def projCalc(ctx, *, args):
     await ctx.send(embed=embed)
 
 @bot.command(description="Restarts The Bot's source file, use if bot freezes etc, [OWNER]")
-async def reboot(ctx):
+async def shutdown(ctx):
     if ctx.author.id == "436646726204653589" or "525334420467744768" or "218142353674731520":
         embed = discord.Embed(color = 0xff0000)
-        embed.add_field(name="Reboot Command Sent, Bot Rebooting in 3 seconds", value = str, inline = False)
+        embed.add_field(name="Shutdown Command Sent, Bot Rebooting in 3 seconds", value = str, inline = False)
         await ctx.send(embed=embed)
         await asyncio.sleep(3)
         os.execl(sys.executable, sys.executable, * sys.argv)
+        await bot.close()
+        os.system("py -3 theBot.py")
 
 bot.run(TOKEN)
