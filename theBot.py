f4817beb70490ca8aa55424507776beb457baf5c
SSwiftGamer440 <37124896+SSwiftGamer440@users.noreply.github.com>
2020-02-06 14:13:45 -0800

    Update theBot.py

--- theBot.py
+++ theBot.py
@@ -258,11 +258,17 @@ async def projCalc(ctx, *, args):
 
 @bot.command(description="Restarts The Bot's source file, use if bot freezes etc, [OWNER]")
 async def shutdown(ctx):
-     if ctx.author.id in (525334420467744768, 436646726204653589, 218142353674731520):
-        embed = discord.Embed(color = 0xff0000)
+     if ctx.author.id in (525334420467744768, 436646726204653589, 218142353674731520, 218590885703581699):
+        embed = discord.Embed(color = 0x00ff00)
         embed.add_field(name="Shutdown Command Sent, Bot Rebooting in 3 seconds, this was executed by", value = None, inline = False)
         await ctx.send(embed=embed)
         await asyncio.sleep(3)
         os.execl(sys.executable, sys.executable, * sys.argv)
+
+    else:
+         embed = discord.Embed(color = 0xff0000)
+        embed.add_field(name="Sorry, you aren't allowed to use this command. Are you the admin of the server you are executing this in? DM CodeWritten#4044 to be added to the exceptions list!", value = None, inline = False)
+        await ctx.send(embed=embed)
+
 
 bot.run(TOKEN)
