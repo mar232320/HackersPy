d3a7851719c956b8cd8c87902416b08c47db3079
SSwiftGamer440 <37124896+SSwiftGamer440@users.noreply.github.com>
2020-02-18 07:49:17 -0800

    Update theBot.py

--- theBot.py
+++ theBot.py
@@ -63,6 +63,7 @@ async def on_ready():
             game = discord.Game(presencelist[i])
             await bot.change_presence(status=discord.Status.online, activity = game)
             await asyncio.sleep(120)
+    
     channel= client.get_channel(679214195119620117)
     await channel.send('Bot Boottime was passed, Bot Online')
     while True:
