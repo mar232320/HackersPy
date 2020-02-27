6d7662064929513f4c8b0d9a6017197869fda9f3
Đức Anh Trần <tranducanh2002@gmail.com>
2020-02-27 12:59:45 +0700

    official fix for the async def on_ready()
    The error was so simple.. a ) is missing on the end of the previous
    sentence

--- theBot.py
+++ theBot.py
@@ -8,7 +8,6 @@ import sys
 import datetime
 import CalculateLib
 import logging
-from itertools import cycle
 from discord.ext import tasks
 
 
@@ -24,6 +23,12 @@ logChannel = bot.get_channel(681216619955224583)
 async def on_ready():
     print("Up and running")
     botStatusLoop.start()
+
+@tasks.loop(seconds = 5)
+async def botStatusLoop():
+    serverlist = discord.Game['Playing with {} guilds'.format(len(bot.guilds))]
+    await bot.change_presence(status=discord.Status.online, activity = discord.Game(serverlist))
+    
 
 @bot.event
 async def on_message(message):
@@ -397,11 +402,6 @@ async def botStatus(ctx, args1):
         game = discord.Game('Playing with {} guilds'.format(len(bot.guilds)))
         await bot.change_presence(status=discord.Status.online, activity=game)
         
-
-@tasks.loop(seconds = 5)
-async def botStatusLoop():
-    game = discord.Game['Playing with {} guilds'.format(len(bot.guilds))]
-    await bot.change_presence(status=discord.Status.online, activity = discord.Game(game)
 
 token = os.environ.get('BOT_TOKEN')
 bot.run(token)
