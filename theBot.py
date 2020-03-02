ddc69789ca5184a4673a5a8ab9c86891f84b7d40
SSwiftGamer440 <37124896+SSwiftGamer440@users.noreply.github.com>
2020-03-01 20:30:18 -0800

    Update theBot.py

--- theBot.py
+++ theBot.py
@@ -418,10 +418,11 @@ async def botStatus(ctx, args1):
         game = discord.Game('Playing with {} guilds'.format(len(bot.guilds)))
         await bot.change_presence(status=discord.Status.online, activity=game)
         
-@bot.commands(hidden= True)
+@bot.command(hidden= True)
 async def guilds(ctx):
-    guildsbot= bot.guilds()
+    guildsbot= bot.guilds
     print (guildsbot)
+    await ctx.send(guildsbot)
                          
                          
 token = os.environ.get('BOT_TOKEN')
